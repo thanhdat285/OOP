3cb353037c3cf63f83f490920c3752335bd35554
Thanh Dat <dd.tt97@gmail.com>
2017-11-12 13:58:40 +0700

    Fix schedule

--- app/controllers/api/v1/customers/schedules_controller.rb
+++ app/controllers/api/v1/customers/schedules_controller.rb
@@ -8,7 +8,7 @@ class Api::V1::Customers::SchedulesController < Api::V1::Customers::BaseControll
     		.paginate(page: params[:page] || 1, per_page: 10)
     elsif params[:film_id].present?
       @schedules = Schedule.select("schedules.*", "users.name as user_sell_name", "films.name as film_name", 
-        "films.image as film_image", "films.kind as film_kind").joins(:user_sell).joins(:film)
+        "films.image as film_image", "films.kind as film_kind", "locations.name").joins(:user_sell).joins(:film)
         .joins(:location).where(film_id: params[:film_id])
         .paginate(page: params[:page] || 1, per_page: 10)
     end
@@ -22,9 +22,9 @@ class Api::V1::Customers::SchedulesController < Api::V1::Customers::BaseControll
       ticket = get_ticket @seats[i][0], @seats[i][1]
       next unless ticket.present?
       if ticket.user_buy_id.present?
-        @seats[i] += [true, ticket.price]
+        @seats[i] += [true, ticket.price, ticket.id]
       else
-        @seats[i] += [false, ticket.price]
+        @seats[i] += [false, ticket.price, ticket.id]
       end
     end
   end
