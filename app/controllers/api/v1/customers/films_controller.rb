a9ea7b48ba2cf62cdc1a58fbca9bd06f9c6ba30a
Thanh Dat <dd.tt97@gmail.com>
2017-12-08 11:14:21 +0700

    return all data

--- app/controllers/api/v1/customers/films_controller.rb
+++ app/controllers/api/v1/customers/films_controller.rb
@@ -4,7 +4,6 @@ class Api::V1::Customers::FilmsController < Api::V1::Customers::BaseController
 
   def index
     @films = Film.select(:id, :name, :image, :kind, :duration, :release_date).all
-      .paginate(page: params[:page] || 1, per_page: 10)
   end
 
   def show
--- app/controllers/api/v1/customers/locations_controller.rb
+++ app/controllers/api/v1/customers/locations_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::Customers::LocationsController < Api::V1::Customers::BaseControll
 	skip_before_action :authenticate_request!
 
   def index
-    locations = Location.all.select(:id, :name).paginate(page: params[:page] || 1, per_page: 10)
+    locations = Location.all.select(:id, :name)
     render json: {code: 1, data: locations}
   end
 end
